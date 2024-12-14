a6d0110ade1c7d0a239e8a7fa71c5c045da63011
vincentime147 <vincentime147@gmail.com>
2024-12-14 01:30:21 +0100

    [ADD] Get Position (page Meteo)

--- screens/meteoScreen.js
+++ screens/meteoScreen.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
-const MeteoScreen = () => {
+export default function MeteoScreen() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [actualWeather, setActualWeather] = useState(null);
+  const [weatherOfTheWeek, setWeatherOfTheWeek] = useState(null);
+////////////////////////Localisation//////////////////////////////////////////////////////////
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting for location...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+  }
+
   return (
-    <View>
-      <Text>Page 2 : Meteo</Text>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>Meteo</Text>
+        <Text>{text}</Text>
+        <Button color="red" title="Refresh" onPress={() => Location.getCurrentPositionAsync({})} />
+      </View>
+      
     </View>
+    
   );
-};
+}
 
-export default MeteoScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
